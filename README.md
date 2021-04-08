0e27edae38b5ffad05fc63ce67d2208c44303890
Johannes 4 Linux <johannesr8484@gmail.com>
2021-04-08 21:34:08 +0200

    Now we can access the IO space of the device!

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ This repository contains a simple Linux Kernel Module for a PCI to Parallel Port
 - pci_parport.c: Sourcefile for LKM
 - Makefile: To build and compile LKM
 
+# Functionality
+
+I have written this code for a PCI to Parallel Port Adapter to which I have connected a little board. On the Board a 7 Segment display is connected to the Data Out Pins. 5 DIP-Switches are connected to the Inputs of the port. The Linux Kernel Module reads in the state of the DIP-switches every 200ms and then sets the 7 segment display according to the DIP's value. 
+
+But the purpose of this Linux Kernel Module is to show you how to access a PCI device and its IO space.
+
 # Dependencies
 
 Make sure, you have the kernel headers and binutils (make, gcc, ...) installed
--- pci_parport.c
+++ pci_parport.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/pci.h>
+#include <linux/jiffies.h>
+#include <linux/timer.h>
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
@@ -13,6 +15,33 @@ MODULE_DESCRIPTION("A simple LKM for a PCI to parallel port adapter");
 /** PCI device */
 static struct pci_dev *ptr;
 
+/** variable for timer */
+static struct timer_list my_timer;
+
+/** Pointer to PCI device's BAR0 */
+static void __iomem * parport_bar0;
+
+/**
+ * @brief Timer Callback Function
+ *
+ * Reads in Inputs from Parport and 
+ * writes value in 7 segment display
+ */
+void timer_callback(struct timer_list * data) {
+	u8 regval;
+
+	/* 7 Segment characters */
+	u8 display[] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111, 119, 124, 57, 94, 121, 113};
+
+	/* Read DIP switches */
+	regval = ioread8((parport_bar0 + 1)) >> 3;
+
+	/* Writing to 7 segment display */
+	iowrite8(display[regval & 0xf], parport_bar0);
+
+	/* Restart Timer */
+	mod_timer(&my_timer, jiffies + msecs_to_jiffies(200));
+}
 /**
  * @brief This function is called, when the module is loaded into the kernel
  */
@@ -38,6 +67,18 @@ static int __init ModuleInit(void) {
 	pci_read_config_word(ptr, PCI_DEVICE_ID, &val);
 	printk("pci_parport - DEVICE ID: 0x%x\n", val);
 
+	/* Request and Map BAR0 */
+	if(pci_request_region(ptr, 0, "pci_parport_bar0")){
+		printk("pci_parport - Could not request BAR0! MAybe already used?\n");
+		return -1;
+	}
+
+	parport_bar0 = pci_iomap(ptr, 0, pci_resource_len(ptr, 0));
+
+	/* Initialize timer */
+	timer_setup(&my_timer, timer_callback, 0);
+	mod_timer(&my_timer, jiffies + msecs_to_jiffies(200));
+
 	return 0;
 }
 
@@ -46,6 +87,8 @@ static int __init ModuleInit(void) {
  */
 static void __exit ModuleExit(void) {
 	printk("pci_parport - Unload module\n");
+	del_timer(&my_timer);
+	pci_release_region(ptr, 0);
 }
 
 module_init(ModuleInit);
